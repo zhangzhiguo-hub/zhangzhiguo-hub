c835208d9ea229889bbe1e9b4ff21e74ee6568d9
zhangzhiguo-hub <72448809+zhangzhiguo-hub@users.noreply.github.com>
2022-04-04 20:59:21 +0800

    Update MRO_Installed.sh

--- MRO_Installed.sh
+++ MRO_Installed.sh
@@ -1,4 +1,4 @@
-#2022年3月27日  以下是我以非root用户在linux下安装最新版MRO的过程
+#2022年4月4日  以下是我以非root用户在linux下安装最新版MRO的过程
 cd ~
 cat /proc/version #查看系统类型
 wget https://mran.blob.core.windows.net/install/mro/4.0.2/Rhel/microsoft-r-open-4.0.2.tar.gz #本系统为红帽，下载对应版本的MRO，官网链接：https://mran.microsoft.com/download
@@ -9,7 +9,7 @@ conda create -n MRO
 conda activate MRO
 
 #开始手动安装
-cd microsoft-r-open
+cd ~/microsoft-r-open
 rpm2cpio rpm/rhel/microsoft-r-open-mkl-4.0.2.rpm|cpio -idmv
 rpm2cpio rpm/rhel/microsoft-r-open-mro-4.0.2.rpm|cpio -idmv
 rpm2cpio rpm/rhel/microsoft-r-open-sparklyr-4.0.2.rpm|cpio -idmv
@@ -19,13 +19,32 @@ mv ${INSTALL_PREFIX}/lib64/R/lib/*.so ${INSTALL_PREFIX}/lib64/R/backup/lib
 cp ${INSTALL_PREFIX}/lib64/R/backup/lib/libR.so ${INSTALL_PREFIX}/lib64/R/lib
 cp ${INSTALL_PREFIX}/stage/Linux/bin/x64/*.so ${INSTALL_PREFIX}/lib64/R/lib
 
-#配置
-conda install -c conda-forge r-base==4.0.2
-cd ~/miniconda3/envs/MRO/bin
-rm ./R
-ln -s ~/microsoft-r-open/opt/microsoft/ropen/4.0.2/lib64/R/bin/R
-echo R_LIBS_USER="/cluster/home/zhiguo/miniconda3/envs/MRO/lib/R/library" /cluster/home/zhiguo/microsoft-r-open/opt/microsoft/ropen/4.0.2/lib64/R/etc/Renviron #更改库的位置
-conda install r-devtools #这两步不能省，用于配置很多必要文件
+#配置环境
+conda install r-base==4.0.2
+cd ~/miniconda3/envs/MRO/lib/R/
+cp -r ./etc ./etc_2
+cp -r ~/microsoft-r-open/opt/microsoft/ropen/4.0.2/lib64/R/* ./
+cp ./etc_2/* ./etc
+sed -i -e '12 s/^/#/' etc/ldpaths
+sed -i -e '13 s/^/#/' etc/ldpaths
+sed -i -e '14 s/^/#/' etc/ldpaths
+conda install r-matrixStats
+conda install r-polyclip
+conda install r-units
+conda install r-V8
+conda install -c conda-forge r-sf
 
 #测试
 R
+options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
+options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
+update.packages()
+install.package("pacman")
+install.package("BiocManager")
+library(BiocManager)
+install.package("devtools")#可能报错
+BiocManager::install("scRepertoire")#可能报错，此外conda安装的4.0以下的R版本无法安装
+
+#若都不报错则安装成功
+####终于成功
+ggforce
